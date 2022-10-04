8a00a8aa297599a6e7faac6a08bad6c4ab5cc46d
Ezequiel santos silva <110132159+ezica09@users.noreply.github.com>
2022-10-04 16:50:49 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,21 +1,35 @@
-//console.log("Alô Mundo...");
-//alert("Alô Mundo..."); 
+//alert("");
+//console.log("");
 //document.write("");
 
-let tabuada = 2; 
- 
+var tabuada = 8;
 
-function mechame(nome){
-    document.write("<h1>tabuada do " + tabuada</h1>")
-    document.write (tabuada + " x 1 = " + (tabuada*1)+ "<br>");
-    document.write (tabuada + " x 2 = " + (tabuada*2)+ "<br>");
-    document.write (tabuada + " x 3 = " + (tabuada*3)+ "<br>");
-    document.write (tabuada + " x 4 = " + (tabuada*4)+ "<br>");
-    document.write (tabuada + " x 5 = " + (tabuada*5)+ "<br>");
-    document.write (tabuada + " x 6 = " + (tabuada*6)+ "<br>");
-    document.write (tabuada + " x 7 = " + (tabuada*7)+ "<br>");
-    document.write (tabuada + " x 8 = " + (tabuada*8)+ "<br>");
-    document.write (tabuada + " x 9 = " + (tabuada*9)+ "<br>");
-    document.write (tabuada + " x 10= " + (tabuada*10)+ "<br>");
-    document.write ("feita por" + nome);
+function mechama (ezequiel){
+    document.write("<h1>Tabuada do " + tabuada + "</h1>");
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br");
+    document.write("Feita por " + ezequiel);
+    
 }
+
+
+function escreva () {
+     for(var i = 1; i < 10; i++){ 
+          document.write(tabuada + " x " + i + " = " +(tabuada*i)+"<br");
+     }
+    
+    }
+
+    function quadrado (){
+        for(var i = 2; i < 21; i++){
+            document.write('o quadrado de'+ i + " é " + (i*i)+ "<br>")
+        }
+    }
