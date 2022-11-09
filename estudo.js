05d5e02e018d04a6434bc3284bdc10a55beba908
Ezequiel santos silva <110132159+ezica09@users.noreply.github.com>
2022-11-09 18:31:55 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,34 +1,55 @@
-"use strict";
-
-let button = document.getElementById('calcular');
-let button_limpar = document.getElementById('limpar');
-
-button.addEventListener('click',calculo);
-button_limpar.addEventListener('click',limpar);
-
-function calculo(e){
-    e.preventDefault();//para impedir que a página HTML sofra refresh
-    let num1 = parseFloat(document.getElementById('num1').value);
-    let num2 = parseFloat(document.getElementById('num2').value);
-    let num3 = parseFloat(document.getElementById('num3').value);
-    let delta = Math.pow(num2,2) - 4*num1*num3;
-    alert(`O delta é: ${delta}`);
-    if(delta > 0){
-        let raiz1 = (-num2 + Math.sqrt(delta) )/(2*num1);
-        let raiz2 = (-num2 - Math.sqrt(delta) )/(2*num1);
-        document.getElementById("resultado").innerHTML = "Raiz 1 = " + raiz1 + "<br>" + "Raiz 2 = " + raiz2; 
-        
-    }else if (delta===0){
-            let raizUnica = (-num2 + Math.sqrt(delta) )/(2*num1);
-            document.getElementById("resultado").innerHTML = "Raiz única = " + raizUnica;
-    }else if (delta < 0){
-        document.getElementById("resultado").innerHTML = "Delta < 0, não existem raízes no conjunto dos números reais.";
-    }
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
 }
+function total(){
+    let valor = document.getElementById("val").value;
+    let juros = document.getElementById("ju").value;
+    let mes = document.getElementById("mes").value;
+    if(!Number(valor)){
+            alert("O valor deve ser um número.");
+            document.getElementById("val").value = "";
+            document.getElementById("val").focus();
+            return
+    } 
+    if(!Number(juros)){
+            alert("O valor de juros deve ser um número.");
+            document.getElementById("ju").value = "";
+            document.getElementById("ju").focus();
+            return
+    } 
+    if(!Number(mes)){
+            alert("A quantidade de meses deve ser um número.");
+            document.getElementById("mes").value = "";
+            document.getElementById("mes").focus();
+            return
+    } 
+    let resultado = 0
+    let texto = "";
+    for(let i = 1; i <= mes; i++){
+            resultado = valor * (1+(juros/100));
+            valor = resultado; 
+             texto += "Mês " + i + " valor " + moeda(resultado)+ "<br>";
+            //document.write("Mês" + i + " valor: " + moeda(resultado) + "<br>"); 
+    }
+    document.getElementById("meses").innerHTML = texto;
+    document.getElementById("resultado").innerHTML = "Total: " + moeda(resultado);
+    //document.write("O Resultado é " + moeda(resultado));
+}
+function calculaRaiz(){
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    let c = document.getElementById("c").value;
+    let delta = (Numbe(b))*(Number(b)) - (4*Number);
+    let x1,x2 = "" ; 
+    if (delta < 0){
+            document.getElementById("raiz").innerHTML = "Não tem raiz real :(";
 
-function limpar(e){
-    document.getElementById('num1').value = " ";
-    document.getElementById('num2').value = " ";
-    document.getElementById('num3').value = " ";
-    document.getElementById('resultado').innerHTML = null;
+    }
+    else if (delta == 0){
+            x1 = (-b)/(2*a);
+            document.getElementById("raiz").innerHTML = "X1 = X2 = ";
+    }
+    else{
+    x1 = ((number(-b))) + Math.sqrt(Number(delta))
+    }
 }
